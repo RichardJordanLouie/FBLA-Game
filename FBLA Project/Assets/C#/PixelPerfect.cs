ee0c548c41674473d29844a01709beb6b55e44cb
Stephen Kelvin Justo <stephenjusto24@gmail.com>
2017-08-12 13:32:03 -0700

    Player
    -Added Patrick (use this as substitute for player for now)

---
+++ FBLA Project/Assets/C#/PixelPerfect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfect : MonoBehaviour {
+
+	// This code helps make the game pixel perfect
+	// If you don't know what that is google it
+	// but in short, pixel perfect allows the art
+	// in the game too look like what the artist intends
+	// it too look
+	// - Stephen
+
+	void Start () {
+		Camera cam = GetComponent<Camera> ();
+		if (cam.orthographic) {
+			cam.orthographicSize = Screen.height / 100f / 2f;
+		}
+	}
+}
